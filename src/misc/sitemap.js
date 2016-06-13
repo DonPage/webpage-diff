e0f4d3d13cb5adf013028f4771c391b254d9e59a
Donald Page <don.page.work@gmail.com>
2016-06-13 12:10:13 -0400

    CHANGED: sitemap

--- src/misc/sitemap.js
+++ src/misc/sitemap.js
@@ -3,29 +3,5 @@ var env = require('../config/environment/index');
 exports.home = env.urls.homepage;
 exports.sitemap = {
     home: '/',
-    // news: '/news',
-    // events: '/events',
-    // employee: {
-    //   index: '/employee-spotlight',
-    //   stories: () => `${sitemap.employee.index}/employee-stories`,
-    //   featuredDepartments: () => `${sitemap.employee.index}/departments`,
-    //   patientStories: () => `${sitemap.employee.index}/patient-stories`,
-    //   why: () => `${sitemap.employee.index}/why-florida-hospital`,
-    //   photography: () => `${sitemap.employee.index}/fhotography`
-    // },
-    // services: {
-    //   index: '/content/services-0',
-    //   transportation: `/services/transportation-and-map`,
-    //   campusDining: `/services/campus-dining`,
-    //   shops: `/services/retail-and-shops`,
-    //   campusResources: `/services/campus-resources`,
-    // },
-    // forNurses: {
-    //   index: '/for-nurses',
-    //   governanceCouncil: () => `${sitemap.forNurses.index}/nurse-governance-council`,
-    //   practiceCouncil: () => `/content/nurse-practice-council-under-construction`
-    // },
-    // about: '/about',
-    // contact: '/contact',
     404: '/404'
 };
